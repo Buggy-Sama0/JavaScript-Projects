b9f1d84c9a369fb4ee7eaf7663937f8922c6783e
Buggy-Sama0 <157221907+Buggy-Sama0@users.noreply.github.com>
2024-08-27 13:40:01 +0800

    complete project

--- To-doList/script.js
+++ To-doList/script.js
@@ -1,27 +1,53 @@
 
-const x=document.getElementById("input");
+const input=document.getElementById("input");
 const list=document.getElementById("list-Task");
+let itemArray=localStorage.getItem("items")?
+JSON.parse(localStorage.getItem("items")):[];
+
+itemArray.forEach(addTask);
+
+function addTask(text) {
+    let li=document.createElement("li");
+    li.innerHTML=text;     
+    list.appendChild(li);
+
+    
+    let button=document.createElement("button");
+    button.innerHTML="X";
+    li.appendChild(button);
+
+    button.onclick=function() {
+        li.remove();
+    }
+
+
+}
+
 
 submit=()=> {
-    if (x.value!=="") {
-        let li=document.createElement("li")
-        li.innerHTML=x.value;
-        list.appendChild(li);
-
-        let button=document.createElement("button");
-        button.innerHTML="X";
-        li.appendChild(button);
-
-        
+    if (input.value!=="") {
+        itemArray.push(input.value);
+        localStorage.setItem("items",JSON.stringify(itemArray));
+        addTask(input.value);
 
     } else {window.alert("Task is empty");}
 
-    x.value="";  
+    input.value="";  
 }
+
+clearAll=()=>{
+    localStorage.clear();
+    list.innerHTML="";
+    itemArray=[];
+
+}
+
+/*
 
 list.addEventListener("click", function(e) {
     if (e.target.tagName==="BUTTON") {
+        localStorage.clear();
         e.target.parentElement.remove();
     }
 });
- 
+ */
