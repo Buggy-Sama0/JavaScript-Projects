1b080d75bd0dfbe43c77795b8aa80d2c5842bc4f
Buggy-Sama0 <157221907+Buggy-Sama0@users.noreply.github.com>
2024-09-01 13:47:38 +0800

    updates

--- Calulator App/script.js
+++ Calulator App/script.js
@@ -2,8 +2,12 @@
 
 const operands=document.getElementsByClassName("operands")
 const display=document.getElementById("display-board");
+const restart=document.getElementById("restartButton");
+const operators=document.getElementsByClassName("operator");
 
 
+
+// The event for displaying operands
 for(let operand of operands) {
     operand.addEventListener("click",function() {
         let para=document.createElement("p");
@@ -11,14 +15,31 @@ for(let operand of operands) {
         para.style.color="white";
         para.style.fontSize="1.9em";
         para.style.fontWeight="bolder";
+  
         
 
         display.appendChild(para);
     })
 }
 
+// The event for displaying operators
+for(let operator of operators) {
+    operator.addEventListener("click",function() {
+        let para=document.createElement("p");
+        para.innerHTML=this.innerHTML;
+        para.style.color="rgb(193, 30, 30)";
+        para.style.fontSize="1.9em";
+        para.style.fontWeight="bolder";
+  
+        display.appendChild(para);
+    })
+}
 
 
+// The event for clearing all the data from display
+restart.addEventListener("click",function() {
+    display.textContent="";
+})
 
 
 
